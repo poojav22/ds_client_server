80bc245428bc0f7f25b0f7c27bcd2f0d356fcfc7
ronyjoe <ronyjoe98@gmail.com>
2024-09-18 18:48:15 -0500

    fix to order response

--- Server/Orders.java
+++ Server/Orders.java
@@ -17,22 +17,17 @@ public class Orders {
 
   }
 
-  List<Order> orderList = new ArrayList<Order>();
+  List<Order> orderList = new ArrayList<>();
   public synchronized List<Order> search(String userName) {
     System.out.println("Searching " + userName);
-    List<Order> searchResult = new ArrayList<Order>();
+    List<Order> searchResult = new ArrayList<>();
     for (Order order: orderList)
       if (userName.equals(order.userName)) searchResult.add(order);
     return searchResult;
   }
-  // returns 0 if old value replaced, otherwise 1
+
   public synchronized boolean insert(int orderId, String userName, String productName, Integer quantity) {
     System.out.println("Inserting " + orderId);
-    for (Order entry: orderList)
-      if (orderId == entry.orderId) {
-        System.out.println("OrderId already exists");
-        return false;
-      }
     orderList.add(new Order(userName, orderId, productName, quantity));
     notifyAll();
     return true;
--- Server/ProductService.java
+++ Server/ProductService.java
@@ -1,7 +1,9 @@
 import java.util.List;
 import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.Collectors;
 
 public class ProductService {
+
   final String INVALID_ITEM_COUNT = "Invalid item quantity provided. cannot order negative items.";
   final String ITEMS_UNAVAILABLE = "Not Available - Not enough items";
   final String INVALID_PRODUCT = "Not Available - We do not sell this product";
@@ -19,55 +21,67 @@ public class ProductService {
     this.orderList = orderList;
     this.orderIdAtomicInteger = orderIdInteger;
   }
-  public String purchase (String userName,String productName,int quantity) {
-    if(quantity < 0)
+
+  public String purchase(String userName, String productName, int quantity) {
+    if (quantity < 0) {
       return INVALID_ITEM_COUNT;
+    }
     String action = "purchase";
     int updateProductTable = table.update(productName, quantity, action);
     switch (updateProductTable) {
-      case -1: return INVALID_PRODUCT;
-      case 0: return ITEMS_UNAVAILABLE;
+      case -1:
+        return INVALID_PRODUCT;
+      case 0:
+        return ITEMS_UNAVAILABLE;
       case 1: {
         int orderId = orderIdAtomicInteger.incrementAndGet();
-        if(orderList.insert(orderId,userName,productName,quantity))
-          return String.format(PURCHASE_SUCCESS, orderId,userName, productName, quantity);
+        if (orderList.insert(orderId, userName, productName, quantity)) {
+          return String.format(PURCHASE_SUCCESS, orderId, userName, productName, quantity);
+        }
         return "Order Insert Failed. Order Id exists";
       }
-      default: return "Result Unknown";
+      default:
+        return "Result Unknown";
     }
   }
 
   public String cancelOrder(int orderId) {
     System.out.println("Initiating Order cancel");
     Orders.Order cancelledOrder = orderList.cancel(orderId);
-    if (cancelledOrder == null)
+    if (cancelledOrder == null) {
       return ORDER_NOT_EXISTS;
+    }
     String action = "cancel";
-    int updateProductTable = table.update(cancelledOrder.productName,cancelledOrder.quantity, action);
+    int updateProductTable = table.update(cancelledOrder.productName, cancelledOrder.quantity,
+        action);
     switch (updateProductTable) {
-      case -1: return INVALID_PRODUCT;
-      case 1: return String.format(CANCEL_SUCCESS, orderId);
-      default: return "Table update Failed";
+      case -1:
+        return INVALID_PRODUCT;
+      case 1:
+        return String.format(CANCEL_SUCCESS, orderId);
+      default:
+        return "Table update Failed";
     }
   }
 
-  public String searchUserOrders(String userName){
-  StringBuffer orderResults = new StringBuffer();
-  List<Orders.Order> searchOrders = orderList.search(userName);
-  if (searchOrders.isEmpty())
-    return String.format(SEARCH_EMPTY, userName);
-  else {
-    for (Orders.Order order: searchOrders){
-      String orderLine = String.format("%d %s %s %d\n", order.orderId, order.productName, order.quantity);
-      orderResults.append(orderLine);
+  public String searchUserOrders(String userName) {
+    StringBuffer orderResults = new StringBuffer();
+    List<Orders.Order> searchOrders = orderList.search(userName);
+    if (searchOrders.isEmpty()) {
+      return String.format(SEARCH_EMPTY, userName);
+    } else {
+      return searchOrders.stream().map(o -> mapOrder(o))
+          .collect(Collectors.joining(System.getProperty("line.separator")));
     }
-    return orderResults.toString();
-  }
   }
 
-  public String listProducts(){
+  private String mapOrder(Orders.Order order) {
+    return String.format("%d %s %d", order.orderId, order.productName, order.quantity);
+  }
+
+  public String listProducts() {
     return table.list();
 
-    }
+  }
 
 }
